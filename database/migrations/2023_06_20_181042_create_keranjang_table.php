79b39bc144b8694dd9cdf641e4f296f053c4307f
Muhamad Sabil Fausta <msabilfaustaa@gmail.com>
2023-06-21 01:23:23 +0700

    fix

---
+++ database/migrations/2023_06_20_181042_create_keranjang_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeranjangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keranjang', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('menu_id');
+            $table->unsignedBigInteger('guest_id');
+            $table->double("total_harga");
+
+            $table->timestamps();
+            $table->foreign('menu_id')->references('id')->on('menu')->onDelete('cascade');
+            $table->foreign('guest_id')->references('id')->on('guest')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keranjang');
+    }
+}
---
+++ database/migrations/2023_06_20_181900_create_pesanan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesananTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("meja_id");
+            $table->double("total_harga");
+
+            $table->timestamps();
+            $table->foreign('meja_id')->references('id')->on('meja')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanan');
+    }
+}
