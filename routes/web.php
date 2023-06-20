b232644a3beeb8b884037030a63d47d463594583
Muhamad Sabil Fausta <msabilfaustaa@gmail.com>
2023-06-20 23:17:28 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// route default
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
